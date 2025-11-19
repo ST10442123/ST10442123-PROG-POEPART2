9a7c36d6bea7892208fdae38c03afb44a95a7b46
ST10442123 <St10442123@vcconnect.edu.za>
2025-11-19 13:01:17 +0200

    Add auto-calculated total on claim submit form

--- obj/Debug/net9.0/CMCS1.AssemblyInfo.cs
+++ obj/Debug/net9.0/CMCS1.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CMCS1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55b43b4b4ca810def138236c3379fe9a2c3f74ec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cebbfb55843b99b1d37119f83552aaa0855e83a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("CMCS1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CMCS1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
